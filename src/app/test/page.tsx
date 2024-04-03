d678b6285bc9db4fcf868d80f875d6629ce9ea73
Ahsinal <lanishakoirala23@gmail.com>
2024-04-03 10:36:57 +0545

    Initiated Project

---
+++ src/app/test/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TestPage = () => {
+  return (
+    <div className='text-xl'>This is unprotected route designed for testing</div>
+  )
+}
+
+export default TestPage
